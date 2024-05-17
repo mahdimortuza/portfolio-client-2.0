05903ca392d2d334ea371dc1b7d45ce3539f1632
mahdimortuza <mahdi.webx@gmail.com>
2024-05-18 00:31:28 +0600

    about added

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,29 @@ import Container from "../../components/container/Container"
 const About = () => {
   return (
     <Container>
-      <div>About</div>
+      <div className="my-10">
+        <h1 className="text-3xl font-semibold"> Hi there...👋! I&apos;m Mahdi Mortuza...,</h1>
+        <p className="text-lg mt-2"> 👉 a versatile Fullstack Web Developer specializing in the MERN stack and Next.js. 
+        I graduated from Bangladesh University of Professionals with a degree in Mass Communication and Journalism, 
+        which means I know how to tell a story and create engaging user experiences—both in print and on the web! 📰➡️💻
+        <br />
+        <br />
+
+After graduation, I dived into the world of web development through Programming Hero&apos;s rigorous bootcamps.
+ In Level 1, I mastered JavaScript, React, HTML, CSS, React Router DOM, and got a taste of Node.js, Express.js, and MongoDB.
+ <br />
+        <br />
+Level 2 was where things got serious. I delved into TypeScript, Mongoose, advanced MongoDB, Express, Node, Redux, modular patterns, 
+MVC architecture, and most important Next.js. I also explored a variety of design libraries like Ant Design, Material-UI, and ShadcnUI,NextUI,  and learned 
+state management with Zustand. Plus, I can add some pizzazz to your projects with web animations! 😉
+<br />
+        <br />
+
+In short, I&apos;m a one-stop shop for creating modern, responsive, and user-friendly web applications. Whether you&apos;re 
+looking for a seamless front-end experience or a robust back-end system, I&apos;ve got you covered. Let&apos;s build something 
+amazing together! 💻🚀
+        </p>
+      </div>
     </Container>
   )
 }
