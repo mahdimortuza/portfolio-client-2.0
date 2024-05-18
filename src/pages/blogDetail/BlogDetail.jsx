2eb6aef178b02be579e65f15d4976f4250a0d33b
mahdimortuza <mahdi.webx@gmail.com>
2024-05-18 12:14:19 +0600

    single blog added

--- src/pages/blogDetail/BlogDetail.jsx
+++ src/pages/blogDetail/BlogDetail.jsx
@@ -1,13 +1,32 @@
 import { useParams } from "react-router-dom"
+import Container from "../../components/container/Container"
+import { useGetSingleBlogQuery } from "../../redux/api/blogApi"
+import ErrorUI from "../../ui/ErrorUI"
+import LoadingUI from "../../ui/LoadingUI"
 
  
 
 const BlogDetail = () => {
     const {id} = useParams()
-    console.log(id)
-
+    const {data, isLoading, isError} = useGetSingleBlogQuery(id)
+    if (isLoading) {
+        return <LoadingUI />;
+      }
+      if (isError) {
+        return <ErrorUI />;
+      }
+      const blog = data.data;
+      console.log(blog)
   return (
-    <div>BlogDetail</div>
+    <Container>
+        <div className="my-10">
+        <img className="w-full h-[300px]" src={blog.image} />
+        <p className="text-sm ">Category: <span>{blog.category}</span></p>
+        <span className='text-xs'>Created at: {blog.createdAt}</span>
+        <h1 className="mt-4 text-2xl md:text-4xl font-bold">{blog.title}</h1>
+        <p className="mt-5 text-lg">{blog.blog}</p>
+        </div>
+    </Container>
   )
 }
 
--- src/redux/api/blogApi.js
+++ src/redux/api/blogApi.js
@@ -3,8 +3,19 @@ import { baseApi } from "./baseApi";
 const blogApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         getAllBlogs: builder.query({
-            query: () => `/blog`
+            query: () => ({
+                url: `/blog`, 
+                method: "GEt"
+            })
+        }), 
+        getSingleBlog: builder.query({
+            query: (id) => ({
+                url: `/blog/${id}`, 
+                method:"GET"
+            })
         })
     })
 })
-export const {useGetAllBlogsQuery} = blogApi
+export const {useGetAllBlogsQuery, useGetSingleBlogQuery} = blogApi
+
+ 
